778188007c70a2c470d6e9679c2a80c6974ce5e6
Kerolos <kerolos.salib@gmail.com>
2019-02-01 10:38:28 +0100

    Add More activities

--- app/src/main/java/com/example/kirfood/ui/activities/LoginActivity.java
+++ app/src/main/java/com/example/kirfood/ui/activities/LoginActivity.java
@@ -2,14 +2,13 @@ package com.example.kirfood.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
